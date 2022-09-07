7e537950393024807d11b01fa0c2de3a29adc202
Jyrgalbek10 <110243095+Jyrgalbek10@users.noreply.github.com>
2022-09-07 21:53:30 +0300

    tap- ta

--- src/Country.java
+++ src/Country.java
@@ -1,2 +1,89 @@
-package PACKAGE_NAME;public class Country {
+import java.util.Comparator;
+
+public class Country{
+    private int id;
+    private String name;
+    private int population;
+    private int gdp;
+
+    public Country(int id, String name, int population, int gdp) {
+        this.id = id;
+        this.name = name;
+        this.population = population;
+        this.gdp = gdp;
+    }
+    public static Comparator<Country> sortCountryPopulation = new Comparator<Country>() {
+        @Override
+        public int compare(Country o1, Country o2) {
+            return o2.population-o1.population;
+        }
+    };
+    public static Comparator<Country>sortCountryId = new Comparator<Country>() {
+        @Override
+        public int compare(Country o1, Country o2) {
+            return o1.id - o2.id;
+        }
+    };
+
+    public static Comparator<Country>sortCountryName = new Comparator<Country>() {
+        @Override
+        public int compare(Country o1, Country o2) {
+            return o1.name.compareTo(o2.name);
+        }
+    };
+
+    public static Comparator<Country>sortCountryGDP= new Comparator<Country>() {
+        @Override
+        public int compare(Country o1, Country o2) {
+            return o2.gdp-o1.gdp;
+        }
+    };
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public int getGdp() {
+        return gdp;
+    }
+
+    public void setGdp(int gdp) {
+        this.gdp = gdp;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", population=" + population +
+                ", gdp=" + gdp +
+                '}';
+    }
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,63 @@
+import java.util.*;
+
 public class Main {
+    static final Random random = new Random();
+    static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        LinkedList<Country> city = new LinkedList<>();
+        Country country0 = new Country(1,"Bishkek",600,11111);
+        Country country1 = new Country(2,"Moscow",700,22222);
+        Country country2 = new Country(3,"Nur-Sultan",800,33333);
+        Country country3 = new Country(4,"Washington",900,44444);
+        Country country4 = new Country(5,"Tashkent",1000,55555);
+        Country country5 = new Country(6,"Minsk",1100,66666);
+        Country country6 = new Country(7,"Ankara",1200,77777);
+        Country country7 = new Country(8,"Baku",1300,88888);
+        Country country8 = new Country(9,"London",1500,99999);
+        Country country9= new Country(10,"Dushanba",1400,10000);
+
+        city.add(country0);
+        city.add(country1);
+        city.add(country2);
+        city.add(country3);
+        city.add(country4);
+        city.add(country5);
+        city.add(country6);
+        city.add(country7);
+        city.add(country8);
+        city.add(country9);
+        System.out.println(" Tapshyrma");
+//        for (Country c : city){
+//
+//            System.out.println(c.getId()+" " + c.getName()+" "+c.getPopulation()+" "+c.getGdp());
+//        }
+//        System.out.println();
+
+        city.sort(Country.sortCountryId);
+        for (Country c: city) {
+            System.out.println(c.getId()+" " + c.getName()+" "+c.getPopulation()+" "+c.getGdp());
+        }
+
+        System.out.println();
+        city.sort(Country.sortCountryName);
+        for (Country c: city) {
+            System.out.println(c.getId() + " " + c.getName() + " " + c.getPopulation() + " " + c.getGdp());
+        }
+
+        System.out.println();
+        city.sort(Country.sortCountryPopulation);
+        for (Country c: city) {
+            System.out.println(c.getId() + " " + c.getName() + " " + c.getPopulation() + " " + c.getGdp());
+
+
+        }
+        System.out.println();
+        city.sort(Country.sortCountryGDP);
+        for (Country c: city) {
+            System.out.println(c.getId() + " " + c.getName() + " " + c.getPopulation() + " " + c.getGdp());
+        }
     }
 }
